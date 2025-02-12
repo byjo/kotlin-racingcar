cff87a2595e72c5680d5eaa2241cd9336f3db007
byjo <buyongjo@gmail.com>
2025-02-13 08:53:31 +0900

    feat: 사용자 입력 값이 빈 문자열, null 일 때 예외 처리

--- src/test/kotlin/calculator/StringCalculatorTest.kt
+++ src/test/kotlin/calculator/StringCalculatorTest.kt
@@ -1,6 +1,6 @@
 package calculator
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.NullAndEmptySource
 
@@ -8,7 +8,7 @@ class StringCalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     fun `입력 값이 null이거나 빈 공백 문자일 경우 예외가 발생한다`(input: String?) {
-        Assertions.assertThatThrownBy {
+        assertThatThrownBy {
             StringCalculator().calculate(input)
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
